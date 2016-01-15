0d5591972563cfa825c26af4959eda884e772ad7
Alex Dao <azd2@duke.edu>
2016-01-15 14:17:27 -0500

    Add Catering problem

---
+++ src/Catering.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * Created by alex on 1/15/16.
+ */
+public class Catering {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            int n = in.nextInt();
+            int k = in.nextInt();
+            if (n == 0 && k == 0) {
+                break;/
+            }
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n - i + 1; j++) {
+
+                }
+            }
+        }
+    }
+}
